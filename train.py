2df5a25afb4f40a4321e036ff221f3816ee655cd
tree-park <hspark.engineer@gmail.com>
2021-02-01 19:56:25 +0900

    error fix

--- train.py
+++ train.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-
-file_dir = os.path.dirname(__file__)
-sys.path.append(file_dir)
+import torch
 
 from lib.util import Config
 from lib.kor2eng import LangTranslator
 from lib.util import load_data
 from lib.data_preprocess import Vocab, preprocessor
 from lib.model.seq2seq import BiLSTMSeq2Seq
-from transformers.lib.model.transformer import Transformer
+from transformer.lib.model.transformer import Transformer
+
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
 
 # load configs
 dconf_path = 'config/data.json'
 mconf_path = 'config/lm.json'
 dconf = Config(dconf_path)
 mconf = Config(mconf_path)
+
+dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+print('Using device:', device)
 
 # load & preprocess corpus
 ko_corpus = preprocessor(load_data(dconf.train_ko_path), lang='ko')
@@ -29,12 +35,14 @@ ko_vocab.load(ko_corpus)
 en_vocab.load(en_corpus)
 
 # define lm model
-if mconf.model == 'seq2seq':
-    model = BiLSTMSeq2Seq(len(ko_vocab) + 1, len(en_vocab) + 1,
-                               mconf.emb_dim, mconf.d_m)
+
 if mconf.model == 'transformer':
     model = Transformer(mconf.d_m, len(ko_vocab) + 1, len(en_vocab) + 1,
-                        mconf.d_m*4, n_layer=3)
+                        mconf.d_m * 4, n_layer=3, device=device)
+else:
+    model = BiLSTMSeq2Seq(len(ko_vocab) + 1, len(en_vocab) + 1,
+                          mconf.emb_dim, mconf.d_m, device=device)
+model.to(device)
 
 # load translator and train
 lm = LangTranslator(model, ko_vocab, en_vocab, dconf, mconf)
